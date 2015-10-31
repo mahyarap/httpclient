1cfd370e974b470fa94f6fd891498366b845af48
Mahyar Abbas Pour <mahyar.abaspour@gmail.com>
2015-10-31 21:51:15 +0330

    Add basic parsing of command line options

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-from httpclient.httpclient import main
+import unittest
+import argparse
+import httpclient.httpclient as httpc
 
-main()
+class CommandLineOptionsTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = argparse.ArgumentParser()
 
+    def tearDown(self):
+        pass
+
+    def test_can_parse_commandline_options(self):
+        args = httpc.parse_cmd_options(['http://localhost'])
+        self.parser.add_argument('url')
+        self.assertEqual(args, 
+            self.parser.parse_args(['http://localhost']))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- httpclient/httpclient.py
+++ httpclient/httpclient.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import sys
+import argparse
 import requests
 
+def parse_cmd_options(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='URL to send the request to')
+
+    return parser.parse_args(args)
 
 def main():
-    pass
+    args = parse_cmd_options(sys.argv[1:])
+    # print('Hello')
 
 
 if __name__ == '__main__':
