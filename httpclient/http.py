a56ee36f39ee528745d58c8a7f18170c720a84ff
Mahyar Abbas Pour <mahyar.abaspour@gmail.com>
2016-01-07 00:08:00 +0330

    Various code improvements in HttpRequest

--- httpclient/http.py
+++ httpclient/http.py
@@ -34,22 +34,26 @@ class HttpMsg(object):
 
 class HttpRequest(HttpMsg):
     def __init__(self, url, method='GET', headers=None, body=None):
-        self.url = url
         self.method = method
-        host, resource = HttpRequest.parse_url(url)
+        host, resource = HttpRequest._parse_url(url)
+        self.host = host
+        self.resource = resource
         startln = method + ' ' + resource + ' ' + 'HTTP/1.1'
         super().__init__(startln, headers, body)
 
     @staticmethod
-    def parse_url(url):
+    def _parse_url(url):
+        parsed_url = []
         if url.startswith('http://'):
-            url = url[7:].split('/', maxsplit=1)
+            parsed_url = url[7:].split('/', maxsplit=1)
         else:
-            url = url.split('/', maxsplit=1)
-        host = url[0]
+            parsed_url = url.split('/', maxsplit=1)
+
+        host = parsed_url[0]
         resource = '/'
-        if len(url) == 2:
-            resource = '/' + url[1]
+        if len(parsed_url) == 2:
+            resource = '/' + parsed_url[1]
+
         return host, resource
 
     @property
@@ -58,41 +62,34 @@ class HttpRequest(HttpMsg):
 
     @method.setter
     def method(self, method):
-        assert method.upper() in ['GET', 'POST', 'PUT', 'DELELTE', 
-            'HEAD', 'TRACE', 'OPTIONS'], 'Invalid request method'
+        assert method.upper() in ('GET', 'POST', 'PUT', 'DELELTE',
+            'HEAD', 'TRACE', 'OPTIONS'), 'Invalid request method'
         self.__method = method
 
     def build(self):
-        host, resource = HttpRequest.parse_url(self.url)
-        request_line = (self.method + 
-            ' ' + resource + 
-            ' ' + 'HTTP/1.1' + 
-            '\r\n')
+        request_line = ' '.join([self.method,
+                                self.resource,
+                                'HTTP/1.1',
+                                '\r\n'])
 
-        host = 'HOST: {0}\r\n'.format(host)
-
+        host = 'HOST: {0}\r\n'.format(self.host)
         headers = ''
         for k, v in self.headers.items():
             headers += '{0}: {1}\r\n'.format(k, v)
         headers += '\r\n'
 
-        header_section = request_line + host + headers
-
-        # body = bytes(body, 'UTF-8')
-
-        request = bytes(header_section, 'UTF-8')
+        request = request_line + host + headers
+        request = bytes(request, 'UTF-8')
         return request
 
     def send(self):
-        host, resource = HttpRequest.parse_url(self.url)
-        conn = Connection(host)
+        conn = Connection(self.host)
         conn.establish()
         request = self.build()
         conn.write(request)
         response = conn.read()
         conn.close()
-        http_resp = HttpResponse(response)
-        return http_resp
+        return HttpResponse(response)
 
 
 class HttpResponse(HttpMsg):
